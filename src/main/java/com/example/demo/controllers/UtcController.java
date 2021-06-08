d953973f9c5d17a1d459efb657111631d6dfbda2
Robcartome <robert.tm2018@gmail.com>
2021-06-08 10:26:59 -0500

    Added function private for response Data

---
+++ src/main/java/com/example/demo/controllers/UtcController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controllers;
+
+import java.time.*;
+import java.time.format.*; 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.models.DateModel;
+import org.springframework.http.*;
+
+@RestController
+@RequestMapping("/api")
+public class UtcController {
+
+  @RequestMapping("/")
+  public String hola(){
+    return "Hola Mundo";
+  }
+
+  
+  @PostMapping("/utc")
+  public ResponseEntity<Object> horaToUtc(@RequestBody DateModel date){
+    ZoneId zonaHoraria = ZoneId.of(date.getZona());
+    String strDate = date.getFecha();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    LocalDateTime localtDateAndTime = LocalDateTime.parse(strDate, formatter);
+    ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(localtDateAndTime, zonaHoraria );
+    ZonedDateTime utcDate = dateAndTimeInSydney.withZoneSameInstant(ZoneOffset.UTC);
+     SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    String horaUtc  = dateFormat.format( Timestamp.valueOf(utcDate.format(formatter)));
+    
+    return new ResponseEntity<Object>(responseData(horaUtc), HttpStatus.OK);
+  }
+
+  // Estructura de respuesta
+  private HashMap responseData(String horaUtc){
+    HashMap<String, HashMap> mapResponse = new HashMap<>();
+    HashMap<String, String> responseData = new HashMap<>();
+    responseData.put("time", horaUtc);
+    responseData.put("timeZone", "utc");
+    mapResponse.put("response", responseData);
+    return  mapResponse;
+  }
+}
---
+++ src/main/java/com/example/demo/models/DateModel.java
@@ -0,0 +1,28 @@
+package com.example.demo.models;
+
+
+// import javax.persistence.*;
+
+public class DateModel {
+
+    private String fecha;
+    private String zona;
+
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+    
+}
